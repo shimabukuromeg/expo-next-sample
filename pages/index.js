615c0a5bb31c50bd7aa65dcfe1d5201b86386369
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2021-11-14 12:17:41 +0900

    actionsheeetのexample追加

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,40 @@ import {
   Button,
   Modal,
   FormControl,
+  Actionsheet,
+  Spacer,
+  useDisclose,
 } from "native-base"
 import { Feather, Entypo } from "@expo/vector-icons"
+
+export function ActionsheetExample() {
+  const { isOpen, onOpen, onClose } = useDisclose()
+  return (
+    <>
+      <Button onPress={onOpen}>Actionsheet</Button>
+      <Actionsheet isOpen={isOpen} onClose={onClose}>
+        <Actionsheet.Content>
+          <Box w="100%" h={60} px={4} justifyContent="center">
+            <Text
+              fontSize="16"
+              color="gray.500"
+              _dark={{
+                color: "gray.300",
+              }}
+            >
+              Albums
+            </Text>
+          </Box>
+          <Actionsheet.Item>Delete</Actionsheet.Item>
+          <Actionsheet.Item>Share</Actionsheet.Item>
+          <Actionsheet.Item>Play</Actionsheet.Item>
+          <Actionsheet.Item>Favourite</Actionsheet.Item>
+          <Actionsheet.Item>Cancel</Actionsheet.Item>
+        </Actionsheet.Content>
+      </Actionsheet>
+    </>
+  )
+}
 
 export const ExampleButton = () => {
   const [showModal, setShowModal] = useState(false)
@@ -173,6 +205,8 @@ export const Example = () => {
         </VStack>
       </VStack>
       <ExampleButton />
+      <Spacer h="10px" />
+      <ActionsheetExample />
     </Box>
   )
 }
