7e8c2e8ee7a6a0ae6ad4d978e538fb6a3a2ef01f
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2021-11-14 12:01:51 +0900

    native-base導入した

--- next.config.js
+++ next.config.js
@@ -3,6 +3,50 @@
 
 const { withExpo } = require('@expo/next-adapter');
 
-module.exports = withExpo({
-  projectRoot: __dirname,
-});
+
+function withFonts(nextConfig = {}) {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      if (!options.defaultLoaders) {
+        throw new Error(
+          "This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade"
+        );
+      }
+
+      const enableSvg = nextConfig.enableSvg || false;
+
+      let testPattern = /\.(woff(2)?|eot|ttf|otf)(\?v=\d+\.\d+\.\d+)?$/;
+
+      if (enableSvg)
+        testPattern = /\.(woff(2)?|eot|ttf|otf|svg)(\?v=\d+\.\d+\.\d+)?$/;
+
+      config.module.rules.push({
+        test: testPattern,
+        // Next.js already handles url() in css/sass/scss files
+        issuer: /\.\w+(?<!(s?c|sa)ss)$/i,
+        use: [
+          {
+            loader: require.resolve("url-loader"),
+            options: {
+              esModule: false,
+              limit: 50000,
+              name: "./static/fonts/[name].[ext]",
+            },
+          },
+        ],
+      });
+
+      if (typeof nextConfig.webpack === "function") {
+        return nextConfig.webpack(config, options);
+      }
+
+      return config;
+    },
+  });
+}
+
+module.exports = withFonts(
+  withExpo({
+    projectRoot: __dirname,
+  })
+)
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,188 @@
-// @generated: @expo/next-adapter@2.1.52
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react"
+import {
+  Input,
+  IconButton,
+  Checkbox,
+  Text,
+  Box,
+  VStack,
+  HStack,
+  Heading,
+  Icon,
+  Center,
+  NativeBaseProvider,
+  Button,
+  Modal,
+  FormControl,
+} from "native-base"
+import { Feather, Entypo } from "@expo/vector-icons"
 
-export default function App() {
+export const ExampleButton = () => {
+  const [showModal, setShowModal] = useState(false)
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome to Expo + Next.js 👋</Text>
-    </View>
-  );
+    <>
+      <Button onPress={() => setShowModal(true)}>Button</Button>
+      <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+        <Modal.Content maxWidth="400px">
+          <Modal.CloseButton />
+          <Modal.Header>Contact Us</Modal.Header>
+          <Modal.Body>
+            <FormControl>
+              <FormControl.Label>Name</FormControl.Label>
+              <Input />
+            </FormControl>
+            <FormControl mt="3">
+              <FormControl.Label>Email</FormControl.Label>
+              <Input />
+            </FormControl>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button.Group space={2}>
+              <Button
+                variant="ghost"
+                colorScheme="blueGray"
+                onPress={() => {
+                  setShowModal(false)
+                }}
+              >
+                Cancel
+              </Button>
+              <Button
+                onPress={() => {
+                  setShowModal(false)
+                }}
+              >
+                Save
+              </Button>
+            </Button.Group>
+          </Modal.Footer>
+        </Modal.Content>
+      </Modal>
+    </>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 16,
-  },
-});
+export const Example = () => {
+  const instState = [
+    {
+      title: "Code",
+      isCompleted: true,
+    },
+    {
+      title: "Meeting with team at 9",
+      isCompleted: false,
+    },
+    {
+      title: "Check Emails",
+      isCompleted: false,
+    },
+    {
+      title: "Write an article",
+      isCompleted: false,
+    },
+  ]
+  const [list, setList] = React.useState(instState)
+  const [inputValue, setInputValue] = React.useState("")
+
+  const addItem = (title: string) => {
+    setList([
+      ...list,
+      {
+        title: title,
+        isCompleted: false,
+      },
+    ])
+  }
+
+  const handleDelete = (index: number) => {
+    const temp = list.filter((_, itemI) => itemI !== index)
+    setList(temp)
+  }
+
+  const handleStatusChange = (index: number) => {
+    const temp = list.map((item, itemI) =>
+      itemI !== index ? item : { ...item, isCompleted: !item.isCompleted }
+    )
+    setList(temp)
+  }
+
+  return (
+    <Box>
+      <Heading >Wednesday</Heading>
+      <VStack mb="5" space={4}>
+        <HStack space={2}>
+          <Input
+            flex={1}
+            onChangeText={(v) => setInputValue(v)}
+            value={inputValue}
+            placeholder="Add Task"
+          />
+          <IconButton
+            borderRadius="sm"
+            variant="solid"
+            icon={
+              <Icon as={Feather} name="plus" size="sm" color="warmGray.50" />
+            }
+            onPress={() => {
+              addItem(inputValue)
+              setInputValue("")
+            }}
+          />
+        </HStack>
+        <VStack space={2}>
+          {list.map((item, itemI) => (
+            <HStack
+              w="100%"
+              justifyContent="space-between"
+              alignItems="center"
+              key={item.title + itemI.toString()}
+            >
+              <Checkbox
+                isChecked={item.isCompleted}
+                onChange={() => handleStatusChange(itemI)}
+                value={item.title}
+              >
+                <Text
+                  mx="2"
+                  strikeThrough={item.isCompleted}
+                  _light={{
+                    color: item.isCompleted ? "gray.400" : "coolGray.800",
+                  }}
+                  _dark={{
+                    color: item.isCompleted ? "gray.400" : "coolGray.50",
+                  }}
+                >
+                  {item.title}
+                </Text>
+              </Checkbox>
+              <IconButton
+                size="sm"
+                colorScheme="trueGray"
+                icon={
+                  <Icon
+                    as={Entypo}
+                    name="minus"
+                    size="xs"
+                    color="trueGray.400"
+                  />
+                }
+                onPress={() => handleDelete(itemI)}
+              />
+            </HStack>
+          ))}
+        </VStack>
+      </VStack>
+      <ExampleButton />
+    </Box>
+  )
+}
+
+export default () => {
+  return (
+    <NativeBaseProvider>
+      <Center flex={1} px="3">
+        <Example />
+      </Center>
+    </NativeBaseProvider>
+  )
+}
